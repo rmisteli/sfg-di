a9dfc204dfaa8150c15abeff2a4cc1e6cb6e9c0d
Reto Misteli <info@rmisteli.ch>
2021-11-01 08:49:10 +0100

    72. Component Scan

--- src/main/java/ch/rmisteli/sfgdi/SfgDiApplication.java
+++ src/main/java/ch/rmisteli/sfgdi/SfgDiApplication.java
@@ -4,12 +4,19 @@ import ch.rmisteli.sfgdi.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"ch.rmisteli.sfgdi", "ch.wog.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println(" ---- The Best Pet is: ");
+		System.out.println(petController.whichPetIsTheBest());
 
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.sayHello());
